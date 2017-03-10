20143832ad1ba95de9400b6197fec0db1df53f66
RedStorm <want2believe@ukr.net>
2017-03-10 21:47:20 +0200

    Version 3000

--- Animal.java
+++ Animal.java
@@ -3,9 +3,5 @@ package lesson01;
 public class Animal {
 	String name="Parent";
 	Animal(){}
-	Animal(String name){
-		this.name=name;
-	System.out.println("dd");
-	System.out.println("ggg");
-	}
+	
 }
